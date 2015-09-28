cecae453f0c94c4cce3326d4f96e5f8f20efb8cc
Skooven <afonso.jordann@gmail.com>
2015-09-28 12:41:46 +0200

    Update:1

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
-
-
-var fs = require('fs');
+var fs = require('fs'); // not needed anymore
 var io = require('socket.io')(http);
 
-
-
-app.get('/',function(req,res){
-    res.sendFile(_dirname + '/index.html');
-
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000);
+http.listen(3000);
